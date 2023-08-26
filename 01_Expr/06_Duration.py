3995a8c016daef5316b644606fdd9712bb4eb073
blue-sunnn <143254259+blue-sunnn@users.noreply.github.com>
2023-08-26 16:29:38 +0700

    Create 06_Duration.py

---
+++ 01_Expr/06_Duration.py
@@ -0,0 +1,20 @@
+h1 = int(input())
+m1 = int(input())
+s1 = int(input())
+h2 = int(input())
+m2 = int(input())
+s2 = int(input())
+
+t1 = (h1 * 3600) + (m1 * 60) + s1
+t2 = (h2 * 3600) + (m2 * 60) + s2
+
+dt = t2 - t1
+dh = (24 + (dt // 3600)) % 24
+dt -= dh * 3600
+
+dm = (60 + (dt // 60)) % 60
+dt -= dm * 60
+
+ds = (60 + dt) % 60
+
+print(str(dh) + ":" + str(dm) + ":" + str(ds))
