6f485df25d7a644aeddc50dbe4f53fb08998f1a5
blue-sunnn <143254259+blue-sunnn@users.noreply.github.com>
2023-08-28 23:11:22 +0700

    Update 06_AddVector.py

--- 02_StrList/06_AddVector.py
+++ 02_StrList/06_AddVector.py
@@ -1,6 +1,7 @@
 u = input()
 v = input()
 
+# cut (), split string, change it to be float, and keep it in list
 u = list(map(float, u[1:-1].split(', ')))
 v = list(map(float, v[1:-1].split(', ')))
 
