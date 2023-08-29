f015e40dca2e9989119a3a16b87d4dbb51aeab9a
blue-sunnn <143254259+blue-sunnn@users.noreply.github.com>
2023-08-29 14:47:00 +0700

    Create 07_AbbrevNum.py

---
+++ 03_if/07_AbbrevNum.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+if n // (10 ** 9) > 0 :
+  n = n / (10 ** 9)
+  alp = 'B'
+elif n // (10 ** 6) > 0 :
+  n = n / (10 ** 6)
+  alp = 'M'
+elif n // (10 ** 3) > 0 :
+  n = n / (10 ** 3)
+  alp = 'K'
+else :
+  alp = ''
+
+if n > 9 : print(str(round(n)) + alp)
+else : print(str(round(n, 1)) + alp)
