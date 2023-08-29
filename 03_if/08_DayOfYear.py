5c49b11f86626c6e53c9b934e6bf73062f72af88
blue-sunnn <143254259+blue-sunnn@users.noreply.github.com>
2023-08-29 14:53:50 +0700

    Create 08_DayOfYear.py

---
+++ 03_if/08_DayOfYear.py
@@ -0,0 +1,11 @@
+dl = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+d = int(input())
+m = int(input())
+y = int(input()) - 543 # change from B.E. to A.D.
+
+# check leap year 
+if y % 400 == 0 : dl[1] += 1
+elif y % 4 == 0 and y % 100 != 0 : dl[1] += 1
+
+print(sum(dl[:m - 1]) + d)
