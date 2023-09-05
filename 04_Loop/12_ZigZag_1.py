01a074f037c34f45495a254e7f3106466f80c2a3
blue-sunnn <143254259+blue-sunnn@users.noreply.github.com>
2023-09-05 16:03:17 +0700

    Create 12_ZigZag_1.py

---
+++ 04_Loop/12_ZigZag_1.py
@@ -0,0 +1,26 @@
+n = int(input())
+x = []
+y = []
+
+for i in range(n) :
+  s = list(map(int, input().split(' ')))
+  x.append(s[0])
+  y.append(s[1])
+
+c = input()
+
+xsl = []
+ysl = []
+
+for i in range(n) :
+  if i % 2 == 0 :
+    xsl.append(x[i])
+    ysl.append(y[i])
+  else :
+    xsl.append(y[i])
+    ysl.append(x[i])
+
+if c == 'Zig-Zag' :
+  print(min(xsl), max(ysl))
+elif c == 'Zag-Zig' :
+  print(min(ysl), max(xsl))
