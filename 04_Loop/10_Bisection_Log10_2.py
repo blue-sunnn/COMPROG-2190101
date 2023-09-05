21bd1e17681741e98eb08527130f4cf01e51443e
blue-sunnn <143254259+blue-sunnn@users.noreply.github.com>
2023-09-05 16:01:11 +0700

    Create 10_Bisection_Log10_2.py

---
+++ 04_Loop/10_Bisection_Log10_2.py
@@ -0,0 +1,17 @@
+a = float(input())
+
+L = U = 0
+b = a
+
+while b > 0 :
+  b //= 10
+  U += 1
+
+x = (L + U) / 2
+
+while abs(a - (10 ** x)) > (10 ** -10) * max(a, 10 ** x) :
+  if 10 ** x > a : U = x
+  elif 10 ** x < a : L = x
+  x = (L + U) / 2
+
+print(round(x, 6))
