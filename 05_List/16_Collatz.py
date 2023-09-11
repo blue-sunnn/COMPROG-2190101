3f1b3589bf12ecb5ae25b0d55bc659cb6f056ff6
blue-sunnn <143254259+blue-sunnn@users.noreply.github.com>
2023-09-12 00:00:31 +0700

    Create 16_Collatz.py

---
+++ 05_List/16_Collatz.py
@@ -0,0 +1,15 @@
+n = int(input())
+mtl = []
+
+mtl.append(n)
+
+while n != 1 :
+  if n % 2 == 0 : n /= 2
+  else : n = 3 * n + 1
+  mtl.append(int(n))
+
+if len(mtl) > 15 : mtl = mtl[-15:]
+
+for i in mtl[:-1] :
+  print(str(i) + '->', end = '')
+print(str(mtl[-1]))
