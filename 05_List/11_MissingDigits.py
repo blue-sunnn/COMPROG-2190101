e239afa3767e5925658a91fbcc36998b5b6900d6
blue-sunnn <143254259+blue-sunnn@users.noreply.github.com>
2023-09-11 23:34:56 +0700

    Create 11_MissingDigits.py

---
+++ 05_List/11_MissingDigits.py
@@ -0,0 +1,10 @@
+s = input()
+num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+mtl, mtl1 = [], []
+
+[mtl.append(i) for i in s if (i in num) and (i not in mtl)]
+mtl.sort()
+[mtl1.append(i) for i in num if (i not in mtl)]
+
+if len(mtl1) > 0 : print(','.join(mtl1))
+else : print('None')
