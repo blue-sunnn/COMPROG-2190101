d490b0078a1d949a8d7b8d81348cc720096804d1
blue-sunnn <143254259+blue-sunnn@users.noreply.github.com>
2023-09-11 23:44:59 +0700

    Create 12_Nicknames.py

---
+++ 05_List/12_Nicknames.py
@@ -0,0 +1,12 @@
+fname = ['Robert', 'William', 'James', 'John', 'Margaret', 'Edward', 'Sarah', 'Andrew', 'Anthony', 'Deborah']
+nname = ['Dick', 'Bill', 'Jim', 'Jack', 'Peggy', 'Ed', 'Sally', 'Andy', 'Tony', 'Debbie']
+mtl = []
+
+n = int(input())
+
+mtl = [(input()) for _ in range(n)]
+
+for i in mtl :
+  if i in fname : print(nname[fname.index(i)])
+  elif i in nname : print(fname[nname.index(i)])
+  else : print('Not found')
