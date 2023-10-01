4bb171eb166eb72c0a2e11124fef6c699a2ba81f
blue-sunnn <143254259+blue-sunnn@users.noreply.github.com>
2023-10-01 12:13:57 +0700

    Create 11_Plural.py

---
+++ 07_/11_Plural.py
@@ -0,0 +1,13 @@
+plural = ['s', 'x']
+vowels = ['a', 'e', 'i', 'o', 'u']
+
+n = input()
+if n[-1:] in plural or n[-2:] == 'ch':
+    n = n + 'es'
+elif n[-1:] == 'y':
+    if n[-2:-1] in vowels: n = n + 's'
+    else: n = n[:-1] + 'ies'
+else:
+    n = n + 's'
+
+print(n)
