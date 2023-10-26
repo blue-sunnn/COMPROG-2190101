9a38aa46de91622c0578f4308e8039a54cd65bcb
blue-sunnn <143254259+blue-sunnn@users.noreply.github.com>
2023-10-26 09:54:03 +0700

    Create 21_Factorization.py

---
+++ 09_NestedLoop/21_Factorization.py
@@ -0,0 +1,21 @@
+def factor(N):  # N is positive integer greater than 1
+    k = 2
+    temp = []
+    while True:
+        if N % k == 0:
+            N /= k
+            temp.append(k)
+        else:
+            k += 1
+        if N == 1: break
+
+    d = {}
+    for i in temp:
+        if i not in d.keys():
+            d[i] = 1
+        else:
+            d[i] += 1
+    return sorted([[k, v] for k, v in d.items()])
+
+
+exec(input().strip())  # this line is required for grader to work.
