163c16d45d6811c430ab6786a4a51eef29d1db0f
blue-sunnn <143254259+blue-sunnn@users.noreply.github.com>
2023-11-06 21:54:22 +0700

    Create Q2.py

---
+++ Midterm_review/Q2.py
@@ -0,0 +1,9 @@
+import math
+
+X, M, S = input().split()
+x, m, s = float(X), float(M), float(S)
+
+if x > 0 and m != 0 and s > 1:
+    print(round(((2 * math.pi)**(-0.5)) * (s**(-1)) * (math.e**(-0.5 * (((x - m)/s)**2))), 2))
+else:
+    print('Invalid input')
