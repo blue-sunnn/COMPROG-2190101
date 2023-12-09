c7a6e7e826ed67679117e122fcb2411b4f0f723c
blue-sunnn <143254259+blue-sunnn@users.noreply.github.com>
2023-12-09 16:18:02 +0700

    Create 03_if_06_RegisteredMail.py

---
+++ Grader/Week3 (03_if)/03_if_06_RegisteredMail.py
@@ -0,0 +1,8 @@
+n = int(input())
+
+if n > 2000:            print('Reject')
+elif 1000 < n <= 2000:  print(str(58))
+elif 500 < n <= 1000:   print(str(38))
+elif 250 < n <= 500:    print(str(28))
+elif 100 < n <= 250:    print(str(22))
+else:                   print(str(18))
