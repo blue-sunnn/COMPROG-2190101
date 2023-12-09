64516f0795afc4dc394638f33f0f0b531f96a833
blue-sunnn <143254259+blue-sunnn@users.noreply.github.com>
2023-12-09 16:18:26 +0700

    Create 04_Loop_04_Parentheses.py

---
+++ Grader/Week4 (04_Loop)/04_Loop_04_Parentheses.py
@@ -0,0 +1,14 @@
+il = input()
+mtl = [0] * len(il)
+
+for i in range(len(il)):
+    if il[i] == '(':    mtl[i] = '['
+    elif il[i] == '[':  mtl[i] = '('
+    elif il[i] == ')':  mtl[i] = ']'
+    elif il[i] == ']':  mtl[i] = ')'
+    else:               mtl[i] = il[i]
+
+mtl = ''.join(mtl)
+
+if mtl == il: print(il)
+else:         print(mtl)
