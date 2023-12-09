32fc3a4c5260ed0935d9fc2572d316d5ab31d3cc
blue-sunnn <143254259+blue-sunnn@users.noreply.github.com>
2023-12-09 16:17:35 +0700

    Create 02_StrList_07_Decryption.py

---
+++ Grader/Week2 (02_StrList)/02_StrList_07_Decryption.py
@@ -0,0 +1,13 @@
+A2J_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+
+n = input()
+
+a = n[3::7]
+b = n[7::5]
+c = str(int(a) + int(b) + 10000)
+d = c[-4:-1]
+e = str(int(d[0]) + int(d[1]) + int(d[2]))
+e = int(e[-1:]) + 1
+f = A2J_list[e - 1]
+
+print(d + f)
