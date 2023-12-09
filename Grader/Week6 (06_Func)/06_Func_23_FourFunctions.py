2b1849d332e4cce66a8f15f32e62c2419cab15e1
blue-sunnn <143254259+blue-sunnn@users.noreply.github.com>
2023-12-09 21:17:12 +0700

    Create 06_Func_23_FourFunctions.py

---
+++ Grader/Week6 (06_Func)/06_Func_23_FourFunctions.py
@@ -0,0 +1,28 @@
+def make_int_list(x):
+    # x is string and convert it to int in list
+    return list(map(int, x.split()))
+
+
+def is_odd(e):
+    # check if e is odd or not
+    return e % 2 != 0
+
+
+def odd_list(alist):
+    # return list of odd numbers
+    x = []
+    for i in alist:
+        if i % 2 != 0:
+            x.append(i)
+    return x
+
+
+def sum_square(alist):
+    # sum of square of each number in list
+    x = 0
+    for i in alist:
+        x += i ** 2
+    return x
+
+
+exec(input().strip())
