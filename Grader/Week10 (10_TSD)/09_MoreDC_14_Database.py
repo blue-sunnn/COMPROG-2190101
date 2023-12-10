7e1d11ae48b2a08d6fc18625fda8e31a7d30f9df
blue-sunnn <143254259+blue-sunnn@users.noreply.github.com>
2023-12-10 20:58:56 +0700

    Create 09_MoreDC_14_Database.py

---
+++ Grader/Week10 (10_TSD)/09_MoreDC_14_Database.py
@@ -0,0 +1,22 @@
+courses, teachers = {}, {}
+
+f = open(input().strip())
+for line in f:
+    n, id = [i.strip() for i in line.split(',')]
+    courses[n] = id
+f.close()
+
+f = open(input().strip())
+for line in f:
+    n, name = [i.strip() for i in line.split(',')]
+    teachers[n] = name
+f.close()
+
+f = open(input().strip())
+for line in f:
+    x, y = [i.strip() for i in line.split(',')]
+    if x in courses and y in teachers:
+        print(courses[x] + ',' + teachers[y])
+    else:
+        print('record error')
+f.close()
