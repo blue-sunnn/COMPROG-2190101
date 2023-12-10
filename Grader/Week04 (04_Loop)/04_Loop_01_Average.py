369b37ee83945e1e96acedf0de3d83e1f2068b15
blue-sunnn <143254259+blue-sunnn@users.noreply.github.com>
2023-12-10 21:07:02 +0700

    Create 04_Loop_01_Average.py

---
+++ Grader/Week04 (04_Loop)/04_Loop_01_Average.py
@@ -0,0 +1,12 @@
+q = ''
+s, i = 0, 0
+
+while q != 'q':
+    q = input()
+    if q == 'q': break
+    else:
+        s += float(q)
+        i += 1
+
+if i > 0: print(round(s / i, 2))
+else:     print('No Data')
