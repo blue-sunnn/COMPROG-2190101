941d4d9e073216f756a92e8b784b93fbed5694c2
blue-sunnn <143254259+blue-sunnn@users.noreply.github.com>
2023-12-10 20:59:41 +0700

    Create 10_NumPy_12_Scalar_and_Array.py

---
+++ Grader/Week12 (11_NumPy)/10_NumPy_12_Scalar_and_Array.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def toCelsius(f):
+    # f = [temperature in Fahrenheit, …]
+    return (f - 32) / (1.8)
+
+
+def BMI(wh):
+    # [[w1,h1],[w2,h2], …]
+    bmi = wh[::, :1:] / ((wh[::, 1::] * 0.01) ** 2)
+    return bmi.T[0]
+
+
+def distanceTo(p, P):
+    # distance from p to all points in P
+    X = P[:, 0]
+    Y = P[:, 1]
+    dx = p[0] - X
+    dy = p[1] - Y
+    return (dx ** 2 + dy ** 2) ** 0.5
+
+
+exec(input().strip())  # must have this line when submitting to grader
