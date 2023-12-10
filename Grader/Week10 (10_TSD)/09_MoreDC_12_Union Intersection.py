d1fff82d853c83619b001cd7981cc92720b9cb2f
blue-sunnn <143254259+blue-sunnn@users.noreply.github.com>
2023-12-10 20:58:50 +0700

    Create 09_MoreDC_12_Union Intersection.py

---
+++ Grader/Week10 (10_TSD)/09_MoreDC_12_Union Intersection.py
@@ -0,0 +1,13 @@
+temp1, temp2 = set(), set()
+
+for i in range(int(input())):
+    x = set(input().split())
+    if i == 0:
+        temp1 = x
+        temp2 = x
+    else:
+        temp1 = temp1.union(x)
+        temp2 = temp2.intersection(x)
+
+print(len(temp1))
+print(len(temp2))
