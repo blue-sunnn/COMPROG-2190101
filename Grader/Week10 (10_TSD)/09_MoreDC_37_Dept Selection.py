045ae1a017dc436059f0734ec048d3e671d3b958
blue-sunnn <143254259+blue-sunnn@users.noreply.github.com>
2023-12-10 20:59:11 +0700

    Create 09_MoreDC_37_Dept Selection.py

---
+++ Grader/Week10 (10_TSD)/09_MoreDC_37_Dept Selection.py
@@ -0,0 +1,18 @@
+dep_info, stu_info, result = {}, [], {}
+
+for _ in range(int(input())):
+    x = input().split()
+    dep_info[x[0]] = int(x[1])
+
+for _ in range(int(input())):
+    x = input().split()
+    stu_info.append([float(x[1]), x[0], x[2:]])
+
+for score, sid, de in sorted(stu_info)[::-1]:
+    for i in de:
+        if dep_info[i] > 0:
+            dep_info[i] -= 1
+            result[sid] = i
+            break
+
+for i, j in sorted(result.items()): print(i, j)
