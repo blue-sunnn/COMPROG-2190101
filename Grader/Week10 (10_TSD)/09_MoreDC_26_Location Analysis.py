3d2348fe7b8c6a454ca2771b10ef23fe1b1dd8b5
blue-sunnn <143254259+blue-sunnn@users.noreply.github.com>
2023-12-10 20:59:02 +0700

    Create 09_MoreDC_26_Location Analysis.py

---
+++ Grader/Week10 (10_TSD)/09_MoreDC_26_Location Analysis.py
@@ -0,0 +1,19 @@
+info, order, temp = {}, [], []
+
+for i in range(int(input())):
+    ids, place = input().split(': ')
+    order.append(ids)
+    info[ids] = place.split(', ')
+
+n = input()
+for i in info[n]:
+    for k, v in info.items():
+        for j in v:
+            if i == j and k != n:
+                temp.append(k)
+
+if len(temp) == 0:
+    print('Not Found')
+else:
+    for i in order:
+        if i in temp: print(i)
